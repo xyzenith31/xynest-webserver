2dc8ff6904467789fde3601200c444e3eaf89f88
donnyindra31 <m.donnyindraprayuda@gmail.com>
2026-05-30 00:11:34 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 flex items-center justify-center text-white">
+      <div className="bg-slate-900 p-10 rounded-2xl border border-slate-800 shadow-2xl">
+        <h1 className="text-4xl font-bold text-cyan-400 mb-4">
+          React + Vite + Tailwind v3
+        </h1>
+
+        <p className="text-slate-300">
+          Setup berhasil bro 🚀
+        </p>
+
+        <button className="mt-6 px-6 py-3 bg-cyan-500 hover:bg-cyan-400 rounded-xl font-semibold text-black transition">
+          Gas Ngoding
+        </button>
+      </div>
+    </div>
+  )
+}
